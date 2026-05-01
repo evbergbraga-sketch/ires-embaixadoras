50f5f0dcf2fcc18dfb31fe7d8ae8aac3d46d9c63
Ruah Systems <evbergbraga@gmail.com>
2026-05-01 12:13:09 -0300

    Update painel.js

--- js/painel.js
+++ js/painel.js
@@ -1847,7 +1847,7 @@ async function renderCapacitacao() {
     });
     covers.forEach(el => {
       const w = el.offsetWidth;
-      if (w > 0) el.style.height = Math.round(w * 1.8) + 'px';
+      if (w > 0) el.style.height = Math.round(w * 2.0) + 'px';
     });
   };
   requestAnimationFrame(() => {
